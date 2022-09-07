26e87c728eb8d222ae89bd51fb5f2cc4ac1436fb
amayuru56789 <indeewara5678@gmail.com>
2022-09-07 10:55:51 +0530

    added about-me & image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';*/
 
 import Header from "./components/header/Header";
+import About from "./components/about/About";
 
 function App() {
   return (
     <>
       <Header/>
+      <About/>
     </>
   );
 }
